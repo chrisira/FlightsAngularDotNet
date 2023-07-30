9fab050c022cab4da4f34b33b47da8890767f6e2
Bishop <christianiradukunda6@gmail.com>
2023-07-30 10:31:40 +0200

    added required interfaces

--- FlightsAngularNet/ClientApp/src/app/search-flights/search-flights.component.ts
+++ FlightsAngularNet/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -7,11 +7,43 @@ import { Component } from '@angular/core';
 })
 export class SearchFlightsComponent {
 
-  SearchResult: any = [
-    'British Airlines',
-    'Kenya Airways',
-    'Rwandair',
-    'Ethiopia Airways'
+  SearchResult: FlightRm[] = [
+    {
+      airline: 'American Airlines',
+      NumberOfRemainingSeats: 500,
+      departure: { time: Date.now().toString(), place: 'Los Angeles' },
+      arrival: { time: Date.now().toString(), place: 'Istanbul' },
+      price: "350"
+    },
+    {
+      airline: 'Rwandair',
+      NumberOfRemainingSeats: 300,
+      departure: { time: Date.now().toString(), place: 'Kigali' },
+      arrival: { time: Date.now().toString(), place: 'Kenya' },
+      price: "1200"
+    },
+    {
+      airline: 'Kenya Airways',
+      NumberOfRemainingSeats: 230,
+      departure: { time: Date.now().toString(), place: 'Nairobi' },
+      arrival: { time: Date.now().toString(), place: 'Kigali' },
+      price: "1250"
+    }
+    
   ]
 
+}
+
+export interface FlightRm {
+  airline: string;
+  arrival: TimeplaceRm;
+  departure: TimeplaceRm;
+  price: string;
+  NumberOfRemainingSeats: number;
+
+
+}
+export interface TimeplaceRm {
+  place: string;
+  time: string;
 }
