ebb692cdfc068e764052d05c5ae958b6496ec9b8
Bishop <christianiradukunda6@gmail.com>
2023-08-01 06:07:48 +0200

    added the find endpoint to the restful api

--- FlightsAngularNet/Controllers/FlightsController.cs
+++ FlightsAngularNet/Controllers/FlightsController.cs
@@ -10,67 +10,75 @@ namespace FlightsAngularNet.Controllers
     {
       
         private readonly ILogger<FlightsController> _logger;
+        static Random random = new Random();
 
-        public FlightsController(ILogger<FlightsController> logger)
-        {
-            _logger = logger;
-        }
-        Random random = new Random();
-
-        [HttpGet]
-        public IEnumerable<FlightsRm> Search()
-            => new FlightsRm[]
+        private static FlightsRm[] flights = new FlightsRm[]
             {
-                new (   Guid.NewGuid(),
+                new (Guid.NewGuid(),
                 "Rwandair",
                 random.Next(90, 5000).ToString(),
-                new TimePlaceRm("Los Angeles",DateTime.Now.AddHours(random.Next(1, 3))),
-                new TimePlaceRm("Istanbul",DateTime.Now.AddHours(random.Next(4, 10))),
+                new TimePlaceRm("Los Angeles", DateTime.Now.AddHours(random.Next(1, 3))),
+                new TimePlaceRm("Istanbul", DateTime.Now.AddHours(random.Next(4, 10))),
                     random.Next(1, 853)),
-        new (   Guid.NewGuid(),
+        new (Guid.NewGuid(),
                 "Deutsche BA",
                 random.Next(90, 5000).ToString(),
-                new TimePlaceRm("Munchen",DateTime.Now.AddHours(random.Next(1, 10))),
-                new TimePlaceRm("Schiphol",DateTime.Now.AddHours(random.Next(4, 15))),
+                new TimePlaceRm("Munchen", DateTime.Now.AddHours(random.Next(1, 10))),
+                new TimePlaceRm("Schiphol", DateTime.Now.AddHours(random.Next(4, 15))),
                 random.Next(1, 853)),
-        new (   Guid.NewGuid(),
+        new (Guid.NewGuid(),
                 "British Airways",
                 random.Next(90, 5000).ToString(),
-                new TimePlaceRm("London, England",DateTime.Now.AddHours(random.Next(1, 15))),
-                new TimePlaceRm("Vizzola-Ticino",DateTime.Now.AddHours(random.Next(4, 18))),
+                new TimePlaceRm("London, England", DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlaceRm("Vizzola-Ticino", DateTime.Now.AddHours(random.Next(4, 18))),
                     random.Next(1, 853)),
-        new (   Guid.NewGuid(),
+        new (Guid.NewGuid(),
                 "Basiq Air",
                 random.Next(90, 5000).ToString(),
-                new TimePlaceRm("Amsterdam",DateTime.Now.AddHours(random.Next(1, 21))),
-                new TimePlaceRm("Glasgow, Scotland",DateTime.Now.AddHours(random.Next(4, 21))),
+                new TimePlaceRm("Amsterdam", DateTime.Now.AddHours(random.Next(1, 21))),
+                new TimePlaceRm("Glasgow, Scotland", DateTime.Now.AddHours(random.Next(4, 21))),
                     random.Next(1, 853)),
-        new (   Guid.NewGuid(),
+        new (Guid.NewGuid(),
                 "BB Heliag",
                 random.Next(90, 5000).ToString(),
-                new TimePlaceRm("Zurich",DateTime.Now.AddHours(random.Next(1, 23))),
-                new TimePlaceRm("Baku",DateTime.Now.AddHours(random.Next(4, 25))),
+                new TimePlaceRm("Zurich", DateTime.Now.AddHours(random.Next(1, 23))),
+                new TimePlaceRm("Baku", DateTime.Now.AddHours(random.Next(4, 25))),
                     random.Next(1, 853)),
-        new (   Guid.NewGuid(),
+        new (Guid.NewGuid(),
                 "Adria Airways",
                 random.Next(90, 5000).ToString(),
-                new TimePlaceRm("Ljubljana",DateTime.Now.AddHours(random.Next(1, 15))),
-                new TimePlaceRm("Warsaw",DateTime.Now.AddHours(random.Next(4, 19))),
+                new TimePlaceRm("Ljubljana", DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlaceRm("Warsaw", DateTime.Now.AddHours(random.Next(4, 19))),
                     random.Next(1, 853)),
-        new (   Guid.NewGuid(),
+        new (Guid.NewGuid(),
                 "ABA Air",
                 random.Next(90, 5000).ToString(),
-                new TimePlaceRm("Praha Ruzyne",DateTime.Now.AddHours(random.Next(1, 55))),
-                new TimePlaceRm("Paris",DateTime.Now.AddHours(random.Next(4, 58))),
+                new TimePlaceRm("Praha Ruzyne", DateTime.Now.AddHours(random.Next(1, 55))),
+                new TimePlaceRm("Paris", DateTime.Now.AddHours(random.Next(4, 58))),
                     random.Next(1, 853)),
-        new (   Guid.NewGuid(),
+        new (Guid.NewGuid(),
                 "AB Corporate Aviation",
                 random.Next(90, 5000).ToString(),
-                new TimePlaceRm("Le Bourget",DateTime.Now.AddHours(random.Next(1, 58))),
-                new TimePlaceRm("Zagreb",DateTime.Now.AddHours(random.Next(4, 60))),
+                new TimePlaceRm("Le Bourget", DateTime.Now.AddHours(random.Next(1, 58))),
+                new TimePlaceRm("Zagreb", DateTime.Now.AddHours(random.Next(4, 60))),
                     random.Next(1, 853))
 
             };
+
+    public FlightsController(ILogger<FlightsController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<FlightsRm> Search()
+        => flights;
+
+        [HttpGet("id")]
+        public FlightsRm Find(Guid id)
+        => flights.SingleOrDefault(f => f.Id == id);
+
         
     }
 }
