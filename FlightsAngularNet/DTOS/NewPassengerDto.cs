943f341d412e3d1673cde712c753ce7f3648b1e3
Bishop <christianiradukunda6@gmail.com>
2023-08-14 05:12:26 +0200

    added validation to newpassengerDto using data annotations

--- FlightsAngularNet/DTOS/NewPassengerDto.cs
+++ FlightsAngularNet/DTOS/NewPassengerDto.cs
@@ -1,10 +1,20 @@
-﻿namespace FlightsAngularNet.DTOS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightsAngularNet.DTOS
 {
     public record NewPassengerDto
    (
+        [Required]
+        [StringLength(100,MinimumLength =3)]
+        [EmailAddress]
         string Email,
+        [Required]
+        [MinLength(2),MaxLength(100)]
         string FirstName,
+        [Required]
+        [MinLength(2),MaxLength(100)]
         string LastName ,
+        [Required]
          bool Gender
         
         );
