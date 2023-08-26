cbc6f76fe85fb26b9f12a1ecba8d1f8475b1a43e
Bishop <christianiradukunda6@gmail.com>
2023-08-26 22:40:12 +0200

    added the booking controller

---
+++ FlightsAngularNet/Controllers/BookingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightsAngularNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingController : ControllerBase
+    {
+    }
+}
