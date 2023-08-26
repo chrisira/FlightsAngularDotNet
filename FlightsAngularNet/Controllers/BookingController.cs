0e7162f0739a6005d82f1c5ba0e5a02aa266b4dc
Bishop <christianiradukunda6@gmail.com>
2023-08-26 23:10:42 +0200

    added the a new booking endpoint

--- FlightsAngularNet/Controllers/BookingController.cs
+++ FlightsAngularNet/Controllers/BookingController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FlightsAngularNet.Data;
+using FlightsAngularNet.ReadModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlightsAngularNet.Controllers
@@ -7,5 +9,25 @@ namespace FlightsAngularNet.Controllers
     [Route("[controller]")]
     public class BookingController : ControllerBase
     {
+        private readonly Entities _entities;
+        public BookingController(Entities entities)
+        {
+            _entities = entities;
+
+        }
+
+        [HttpGet("{email}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(IEnumerable<BookingRm>), 200)]
+        public ActionResult<IEnumerable<BookingRm>> List(string email)
+        {
+
+
+        }
+
+
     }
 }
