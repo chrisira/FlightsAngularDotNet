91e7fe09d8d180aefab80b51d49cebaa0a26e780
Bishop <christianiradukunda6@gmail.com>
2023-08-26 22:45:54 +0200

    added the BookingRm in the Read Models

---
+++ FlightsAngularNet/ReadModels/BookingRm.cs
@@ -0,0 +1,13 @@
+﻿namespace FlightsAngularNet.ReadModels
+{
+    public record BookingRm
+    (
+        Guid FlighItd,
+        string Airling,
+        string Price,
+        TimePlaceRm Arrival,
+        TimePlaceRm Departure,
+        int NumberOfBookedSeats,
+        string PassengerEmail
+        );
+}
