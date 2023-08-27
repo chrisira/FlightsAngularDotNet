7ff1ed93647b35e539db858cbca0c25315fd0927
Bishop <christianiradukunda6@gmail.com>
2023-08-27 11:01:01 +0200

    regenerated the bookings services to correct the spelling error

--- FlightsAngularNet/ClientApp/src/app/api/models/booking-rm.ts
+++ FlightsAngularNet/ClientApp/src/app/api/models/booking-rm.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 import { TimePlaceRm } from './time-place-rm';
 export interface BookingRm {
-  airling?: null | string;
+  airline?: null | string;
   arrival?: TimePlaceRm;
   departure?: TimePlaceRm;
   flighItd?: string;
