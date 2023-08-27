73af2731ada42161af7914adf2988860cca6970e
Bishop <christianiradukunda6@gmail.com>
2023-08-27 10:45:47 +0200

    implemented the booking service with email as inpu parameter from the authentication service

--- FlightsAngularNet/ClientApp/src/app/my-bookings/my-bookings.component.ts
+++ FlightsAngularNet/ClientApp/src/app/my-bookings/my-bookings.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookingRm } from '../api/models';
+import { BookingService } from '../api/services';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-my-bookings',
   templateUrl: './my-bookings.component.html',
   styleUrls: ['./my-bookings.component.css']
 })
-export class MyBookingsComponent {
+export class MyBookingsComponent implements OnInit {
+
+  bookings!: BookingRm[]
+  constructor(private bookingService:BookingService,private authService : AuthService) {
+
+  }
+
+
+  ngOnInit(): void{
+    this.bookingService.listBooking$Json({
+      email: this.authService.
+      currentUser?.email ?? ''
+    }).subscribe(r => this.bookings = r)
+
+  }
 
 }
